e58f1debe3f03b6cdf41898b98e888609f964c8e
kelcileneBispo <kelciparnaiba@gmail.com>
2020-01-13 19:26:10 -0300

    o começo da aplicação

---
+++ materialize/js/login.js
@@ -0,0 +1,10 @@
+function validar(){
+    var email = form.email.value;
+    var senha = form.senha.value;
+
+    if(email == ""){
+        alert("eeeeee");
+        form.email.focus();
+        return false;
+    }
+}
