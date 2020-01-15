a330dbb95f523b36e29d68a73c3b3413fe4c49a9
kelcileneBispo <kelciparnaiba@gmail.com>
2020-01-15 20:13:34 -0300

    cadastro esta salvando no localStorage

--- materialize/js/cadastro.js
+++ materialize/js/cadastro.js
@@ -1,19 +1,39 @@
-function validar(){
-    var nome = cadastroUsuario.nome.value;
-    var sobrenome = cadastroUsuario.sobrenome.value;
-    var telefone = cadastroUsuario.telefone.value;
-    var cpf = cadastroUsuario.cpf.value;
-    var email = cadastroUsuario.email.value;
-    var endereco = cadastroUsuario.endereco.value;
+document.getElementById('cadastro').addEventListener('submit',cadastraUsuario);
 
-    if(nome == "" || sobrenome == "" || telefone == "" || cpf == "" || email == "" || endereco == ""){
-        alert("prencha os campos corretamente");
-        cadastroUsuario.nome.focus();
-        cadastroUsuario.sobrenome.focus();
-        cadastroUsuario.telefone.focus();
-        cadastroUsuario.cpf.focus();
-        cadastroUsuario.email.focus();
-        cadastroUsuario.endereco.focus();
-        return false;
+function cadastraUsuario(e){
+    var nome = document.getElementById('nome').value;
+    var sobrenome = document.getElementById('sobrenome').value;
+    var telefone = document.getElementById('telefone').value;
+    var cpf = document.getElementById('cpf').value;
+    var email = document.getElementById('email').value;
+    var endereco = document.getElementById('endereco').value;
+    var senha = document.getElementById('senha').value;
+
+    if(!nome || !sobrenome || !telefone || !cpf || !email || !endereco || !senha){
+        alert('por favor, preencha os campos em branco');
     }
+
+    usuario01 = {
+        nomeUser : nome,
+        sobrenomeUser : sobrenome,
+        telefoneUser : telefone,
+        cpfUser : cpf,
+        emailUser : email,
+        enderecoUser : endereco,
+        senhaUser : senha
+    }
+    console.log(usuario01);
+
+    if(localStorage.getItem('usuario') === null){
+        var cliente = [];
+        cliente.push(usuario01);
+        localStorage.setItem('usuario', JSON.stringify(cliente));
+    }
+    else{
+        var cliente = JSON.parse(localStorage.getItem('usuario'));
+        cliente.push(usuario01);
+        localStorage.setItem('usuario',JSON.stringify(cliente));
+    }
+   
+    e.preventDefault();
 }
